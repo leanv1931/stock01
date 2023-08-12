37dba748fcc54fe50fd970dc7418378f05715ee5
leandro valenzuela <leanv1931@gmail.com>
2023-08-12 18:55:55 -0300

    fix structure 2

--- back/producto.js
+++
@@ -1,9 +0,0 @@
-// Definición de la clase Producto
-class Producto {
-    constructor(id, nombre, precio) {
-      this.id = id;
-      this.nombre = nombre;
-      this.precio = precio;
-    }
-  }
-  
--- css/stock-modularizado/index.js
+++
@@ -1,7 +0,0 @@
-import { agregarTarea } from './tareas.js';
-
-// Obtenemos referencias a los elementos del DOM
-const btnAgregar = document.getElementById('btn');
-
-// Agregamos el evento click al botón
-btnAgregar.addEventListener('click', agregarTarea);
